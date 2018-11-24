1f80782a40b5fe3d7b19535e46fb1491a3e36840
Arijit Ganguly <arijit.ganguly004@gmail.com>
2018-11-24 11:58:05 +0530

    Updated

---
+++ GeneticAlgorithmGame/Assets/_Scripts/DNA.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DNA : MonoBehaviour {
+
+    //gene for colour
+    public float r;
+    public float g;
+    public float b;
+    public float s;
+
+    bool dead = false;
+    public float timeToDie = 0;
+
+    SpriteRenderer sRenderer;
+    Collider2D sCollider;
+
+    void Start () {
+        sRenderer = GetComponent<SpriteRenderer>();
+        sCollider = GetComponent<Collider2D>();
+        sRenderer.color = new Color(r, g, b);
+        this.transform.localScale = new Vector3(s, s, s);
+	}
+	
+	void Update () {
+		
+	}
+
+    //Function to run when object is clicked upon
+    void OnMouseDown()
+    {
+        dead = true;
+        timeToDie = PopulationManager.elapsed;
+        sRenderer.enabled = false;
+        sCollider.enabled = false;
+    }
+}
---
+++ GeneticAlgorithmGame/Assets/_Scripts/PopulationManager.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class PopulationManager : MonoBehaviour {
+
+    public GameObject personPrefab;
+    public int populationSize = 10;
+    List<GameObject> population = new List<GameObject>();
+    public static float elapsed = 0;
+    int trialTime = 10;
+    int generation = 1;
+
+	void Start () {
+		for(int i=0; i < populationSize; i++)
+        {
+            Vector3 pos = new Vector3(Random.Range(-9, 9), Random.Range(-4.5f, 4.5f), 0);
+            GameObject go = Instantiate(personPrefab, pos, Quaternion.identity);
+            go.GetComponent<DNA>().r = Random.Range(0.0f, 1.0f);
+            go.GetComponent<DNA>().g = Random.Range(0.0f, 1.0f);
+            go.GetComponent<DNA>().b = Random.Range(0.0f, 1.0f);
+            go.GetComponent<DNA>().s = Random.Range(0.1f, 0.3f);
+            population.Add(go);
+        }
+	}
+	
+	void Update () {
+        elapsed += Time.deltaTime;
+        if(elapsed > trialTime)
+        {
+            BreedNewPopulation();
+            elapsed = 0;
+        }
+		
+	}
+
+    void BreedNewPopulation()
+    {
+        List<GameObject> newPopulation = new List<GameObject>();
+
+        //get rid of unfit individuals
+        List<GameObject> sortedList = population.OrderBy(o => o.GetComponent<DNA>().timeToDie).ToList();
+
+        population.Clear();
+
+        //breed upper half of the sorted list
+        for (int i = (int) (sortedList.Count / 2.0f) - 1; i < sortedList.Count - 1; i++)
+        {
+            population.Add(Breed(sortedList[i], sortedList[i + 1]));
+            population.Add(Breed(sortedList[i + 1], sortedList[i]));
+        }
+
+        //destroy all parents and previous population
+        for(int i = 0; i < sortedList.Count; i++)
+        {
+            Destroy(sortedList[i]);
+        }
+
+        generation++;
+    }
+
+    GameObject Breed(GameObject parent1, GameObject parent2)
+    {
+        Vector3 pos = new Vector3(Random.Range(-9, 9), Random.Range(-4.5f, 4.5f), 0);
+        GameObject offSpring = Instantiate(personPrefab, pos, Quaternion.identity);
+        DNA dna1 = parent1.GetComponent<DNA>();
+        DNA dna2 = parent2.GetComponent<DNA>();
+
+        //swap paret dna
+        //Mutating with 0.5% of time
+        if(Random.Range(0, 1000) < 5)
+        {
+            offSpring.GetComponent<DNA>().r = Random.Range(0, 10) < 5 ? dna1.r : dna2.r;
+            offSpring.GetComponent<DNA>().g = Random.Range(0, 10) < 5 ? dna1.g : dna2.g;
+            offSpring.GetComponent<DNA>().b = Random.Range(0, 10) < 5 ? dna1.b : dna2.b;
+            offSpring.GetComponent<DNA>().s = Random.Range(0, 10) < 5 ? dna1.s : dna2.s;
+        }
+        else
+        {
+            //Creating Mutations of color
+            offSpring.GetComponent<DNA>().r = Random.Range(0.0f, 1.0f);
+            offSpring.GetComponent<DNA>().g = Random.Range(0.0f, 1.0f);
+            offSpring.GetComponent<DNA>().b = Random.Range(0.0f, 1.0f);
+            offSpring.GetComponent<DNA>().s = Random.Range(0.1f, 0.3f);
+        }
+        return offSpring;
+    }
+
+    GUIStyle guiStyle = new GUIStyle();
+    void OnGUI()
+    {
+        guiStyle.fontSize = 50;
+        guiStyle.normal.textColor = Color.white;
+        GUI.Label(new Rect(10, 10, 100, 20), "Generation: " + generation, guiStyle);
+        GUI.Label(new Rect(10, -30, 100, 20), "Trial Time: " + (int)elapsed, guiStyle);
+
+    }
+}
